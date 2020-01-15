d2a5635c091d2cffff9d664851d4e3a974a73c6c
shraga89 <shraga89@gmail.com>
2020-01-15 09:31:58 +0200

    WW

--- HHandler.py
+++ HHandler.py
@@ -33,7 +33,7 @@ class HHandler:
                         .replace(' ', '').lower()
                     last_time = datetime.strptime(last_line_split[0].split(',')[0], '%Y-%m-%d %H:%M:%S')
                 # corr = tuple((last_ai, line_split[8]))
-                curr_ai = line_split[8].split('.')[-2].replace('"', '').replace('@en', '').replace(' ', '').lower()
+                curr_ai = line_split[8].split('.')[-1].replace('"', '').replace('@en', '').replace(' ', '').lower()
                 if 'name of an entity' in line_split[8]:
                     curr_ai = line_split[8].split('.')[-2].replace('"', '').replace('@en', '').replace(' ', '').lower()
                 # elif '@en' in line_split[-2]:
